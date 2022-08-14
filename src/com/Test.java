fda8411456332655aa20b74a7b88a791cc8f54d0
vincent <vincent_ntu@qq.com>
2022-08-14 20:04:32 +0800

    testIDEA

---
+++ src/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @author Vincent
+ * @version 1.0
+ * @date 2022/8/14 20:00
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
